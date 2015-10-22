7af7ad80c559e881a06f7a0c3420959623771309
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-10-22 16:01:57 +0000

    Add javadoc comments to IRouteDAO
    Adds javadoc comments to methods on IRouteDAO interface.

--- src/model/IRouteDAO.java
+++ src/model/IRouteDAO.java
@@ -9,18 +9,55 @@ import java.util.List;
  */
 public interface IRouteDAO {
 
+	/*
+	 * Creates a new record for a given rock climb route
+	 * 
+	 * @param route A new Route instance
+	 */
 	void createRecord(Route route);
 
+	/*
+	 * Retrieves a route record
+	 * 
+	 * @param id The id number of the record to fetch
+	 * @return The route object with the specified routeId
+	 */
 	Route retrieveRecordById(int id);
 
+	/*
+	 * Retrieves all records as a list of Route instances
+	 * 
+	 * @return A list of Route instances
+	 */
 	List<Route> retrieveAllRecords();
 
+	/*
+	 * Updates a given route with a new details
+	 * 
+	 * @param updatedRoute The new route object whose details will write over the db entry, so long as they have the same id.
+	 */
 	void updateRecord(Route updatedRoute);
 
+	
+	/*
+	 * Deletes a single record, specified by id number
+	 * 
+	 * @param routeId The id value of the route to delete
+	 */
 	void deleteRecord(int routeId);
 
+	/*
+	 * Deletes a single record, specified by passing a reference to that particular Route object
+	 * 
+	 * @param route The Route instance to delete. Must exist.
+	 */
 	void deleteRecord(Route route);
 
+	/*
+	 * Converts all records into a concatenated string
+	 * 
+	 * @return A string, obviously.
+	 */
 	String toString();
 
 }
